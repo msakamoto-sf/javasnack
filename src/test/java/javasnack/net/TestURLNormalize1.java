7b8db21075e34e9826067616a7a27e5ed7b23a17
Masahiko Sakamoto <sakamoto.gsyc.3s@gmail.com>
2015-11-06 00:26:32 +0900

    move TestURLNormalize1 package to javasnack.langspecs.net

--- src/test/java/javasnack/net/TestURLNormalize1.java
+++ src/test/java/javasnack/net/TestURLNormalize1.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package javasnack.langspecs;
+package javasnack.net;
 
 import static org.testng.Assert.assertEquals;
 
