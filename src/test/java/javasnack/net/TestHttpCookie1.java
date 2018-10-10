556b764a32392297280b755239f751cf32a2964e
sakamoto <sakamoto@securesky-tech.com>
2018-10-10 19:51:40 +0900

    add HttpCookie.domainMatches() test cases.

---
+++ src/test/java/javasnack/net/TestHttpCookie1.java
@@ -0,0 +1,41 @@
+package javasnack.net;
+
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+
+import java.net.HttpCookie;
+import java.net.URISyntaxException;
+
+import org.testng.annotations.Test;
+
+public class TestHttpCookie1 {
+
+    @Test
+    public void testDomainMatches() throws URISyntaxException {
+        assertFalse(HttpCookie.domainMatches("", "aaa.bbb.com"));
+        assertTrue(HttpCookie.domainMatches("aaa.bbb.com", "aaa.bbb.com"));
+
+        assertFalse(HttpCookie.domainMatches("com", "com"));
+        assertFalse(HttpCookie.domainMatches("com", "xxxcom"));
+        assertFalse(HttpCookie.domainMatches("com", "aaa.com"));
+        assertFalse(HttpCookie.domainMatches(".com", "com"));
+        assertFalse(HttpCookie.domainMatches(".com", "xxxcom"));
+        assertFalse(HttpCookie.domainMatches(".com", "aaa.com"));
+        assertFalse(HttpCookie.domainMatches(".com", "xxxaaa.com"));
+
+        assertFalse(HttpCookie.domainMatches("example", "example"));
+        assertFalse(HttpCookie.domainMatches("example", "example.local"));
+
+        assertTrue(HttpCookie.domainMatches(".local", "example")); // see javadoc
+        assertTrue(HttpCookie.domainMatches(".local", "example.local")); // see javadoc
+
+        assertTrue(HttpCookie.domainMatches("bbb.com", "bbb.com"));
+        assertTrue(HttpCookie.domainMatches("bbb.com", "xxxbbb.com")); // WOW!!
+        assertFalse(HttpCookie.domainMatches("bbb.com", "aaa.bbb.com"));
+
+        assertTrue(HttpCookie.domainMatches(".bbb.com", "bbb.com"));
+        assertFalse(HttpCookie.domainMatches(".bbb.com", "xxxbbb.com"));
+        assertTrue(HttpCookie.domainMatches(".bbb.com", "aaa.bbb.com"));
+        assertFalse(HttpCookie.domainMatches(".bbb.com", "xxx.aaa.bbb.com"));
+    }
+}
