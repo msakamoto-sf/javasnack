ff4adb2a0d5ae377fb35853509941bcf08be4d82
Masahiko Sakamoto <sakamoto.gsyc.3s@gmail.com>
2019-05-19 17:30:25 +0900

    replace hamcrest matcher to junit5 assertions.

--- src/test/java/javasnack/langspecs/TestByteBuffers.java
+++ src/test/java/javasnack/langspecs/TestByteBuffers.java
@@ -15,45 +15,47 @@
  */
 package javasnack.langspecs;
 
-import static org.hamcrest.MatcherAssert.*;
-import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.io.ByteArrayOutputStream;
 import java.io.UnsupportedEncodingException;
+import java.nio.BufferOverflowException;
 import java.nio.ByteBuffer;
 
-import org.testng.Assert;
-import org.testng.annotations.Test;
+import org.junit.jupiter.api.Test;
 
 public class TestByteBuffers {
 
-    @Test(expectedExceptions = { java.nio.BufferOverflowException.class })
+    @Test
     public void bufferOverflowMustOccurr() {
         ByteBuffer b = ByteBuffer.allocate(2);
-        b.put((byte)1);
-        b.put((byte)2);
-        assertThat(b.position(), is(2));
-        b.put((byte)3);
-        Assert.fail();
+        b.put((byte) 1);
+        b.put((byte) 2);
+        assertEquals(2, b.position());
+        assertThrows(BufferOverflowException.class, () -> {
+            b.put((byte) 3);
+        });
     }
 
     @Test
     public void usageOfByteArrayOutputStream() throws UnsupportedEncodingException {
         // Allocate initial size as 2 byte.
         ByteArrayOutputStream o = new ByteArrayOutputStream(2);
-        o.write((byte)0);
-        o.write((byte)1);
-        assertThat(o.size(), is(2));
+        o.write((byte) 0);
+        o.write((byte) 1);
+        assertEquals(2, o.size());
         // Add more 1 byte.
-        o.write((byte)2);
+        o.write((byte) 2);
         // Size must expand 1.
-        assertThat(o.size(), is(3));
+        assertEquals(3, o.size());
 
         // get string
         String s = o.toString("ISO-8859-1");
         // get byte array from string
         byte[] r = s.getBytes("ISO-8859-1");
         // original toByteArray() must be equal to r.
-        assertThat(r, is(o.toByteArray()));
+        assertArrayEquals(o.toByteArray(), r);
     }
 }
--- src/test/java/javasnack/langspecs/TestISO8859.java
+++ src/test/java/javasnack/langspecs/TestISO8859.java
@@ -15,17 +15,17 @@
  */
 package javasnack.langspecs;
 
-import static org.hamcrest.MatcherAssert.*;
-import static org.hamcrest.Matchers.*;
-import static org.testng.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import java.io.UnsupportedEncodingException;
 import java.nio.ByteBuffer;
 
+import org.junit.jupiter.api.Test;
+
 import javasnack.tool.CharsetTool;
 import javasnack.tool.UnsignedByte;
-
-import org.testng.annotations.Test;
 
 public class TestISO8859 {
 
@@ -41,7 +41,7 @@ public class TestISO8859 {
         try {
             String s = new String(src, CharsetTool.BINARY);
             byte[] dst = s.getBytes(CharsetTool.BINARY);
-            assertThat(dst, is(src));
+            assertArrayEquals(src, dst);
         } catch (UnsupportedEncodingException ignore) {
             ignore.printStackTrace();
         }
@@ -59,7 +59,7 @@ public class TestISO8859 {
         try {
             String s = new String(src, CharsetTool.BINARY);
             byte[] dst = s.getBytes(CharsetTool.BINARY);
-            assertThat(dst, is(src));
+            assertArrayEquals(src, dst);
         } catch (UnsupportedEncodingException ignore) {
             ignore.printStackTrace();
         }
@@ -77,7 +77,7 @@ public class TestISO8859 {
         try {
             String s = new String(src, CharsetTool.BINARY);
             byte[] dst = s.getBytes(CharsetTool.BINARY);
-            assertThat(dst, is(src));
+            assertArrayEquals(src, dst);
         } catch (UnsupportedEncodingException ignore) {
             ignore.printStackTrace();
         }
@@ -95,7 +95,7 @@ public class TestISO8859 {
         try {
             String s = new String(src, CharsetTool.BINARY);
             byte[] dst = s.getBytes(CharsetTool.BINARY);
-            assertThat(dst, is(src));
+            assertArrayEquals(src, dst);
         } catch (UnsupportedEncodingException ignore) {
             ignore.printStackTrace();
         }
@@ -113,7 +113,7 @@ public class TestISO8859 {
         try {
             String s = new String(src, CharsetTool.BINARY);
             byte[] dst = s.getBytes(CharsetTool.BINARY);
-            assertThat(dst, is(src));
+            assertArrayEquals(src, dst);
         } catch (UnsupportedEncodingException ignore) {
             ignore.printStackTrace();
         }
@@ -124,7 +124,7 @@ public class TestISO8859 {
         String errs = "こんにちは, これはJapanese Characterです。";
         byte[] dst1 = errs.getBytes(CharsetTool.LATIN1);
         String r = new String(dst1, CharsetTool.LATIN1);
-        assertNotEquals(r, errs);
+        assertNotEquals(errs, r);
     }
 
     @Test
@@ -132,8 +132,8 @@ public class TestISO8859 {
         char[] data = new char[] { 'a', 'b', '\0', 'c', 'd', '\0', 'e', 'f' };
         String data2 = new String(data);
         String expected1 = "ab" + "\0" + "cd" + "\0" + "ef";
-        assertEquals(data2, expected1);
+        assertEquals(expected1, data2);
         String data3 = data2.replaceAll("\0", "");
-        assertEquals(data3, "abcdef");
+        assertEquals("abcdef", data3);
     }
 }
