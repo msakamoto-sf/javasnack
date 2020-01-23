5d064313b90925d61c7add6ea25dfebff434abd3
Masahiko Sakamoto <sakamoto.gsyc.3s@gmail.com>
2020-01-23 23:59:21 +0900

    add some test code to TestStreamCollectorsDemo

--- src/test/java/javasnack/functionlambdastream/TestStreamCollectorsDemo.java
+++ src/test/java/javasnack/functionlambdastream/TestStreamCollectorsDemo.java
@@ -1,13 +1,19 @@
 package javasnack.functionlambdastream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashSet;
 import java.util.IntSummaryStatistics;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -79,20 +85,92 @@ public class TestStreamCollectorsDemo {
         assertThat(Stream.of(1, 2, 3, 4, 5).collect(col1)).isEqualTo(2L);
     }
 
+    @Test
+    public void testToListDemo() {
+        final Collector<String, ?, List<String>> col1 = Collectors.toList();
+        List<String> l1 = Stream.of("aa", "bb", "cc").collect(col1);
+        assertThat(l1).isEqualTo(List.of("aa", "bb", "cc"));
+        l1.add("dd");
+        assertThat(l1).isEqualTo(List.of("aa", "bb", "cc", "dd"));
+    }
+
+    @Test
+    public void testToSetDemo() {
+        final Collector<String, ?, Set<String>> col1 = Collectors.toSet();
+        final Set<String> s1 = Stream.of("aa", "bb", "cc").collect(col1);
+        assertThat(s1).isEqualTo(Set.of("aa", "bb", "cc"));
+        s1.add("dd");
+        assertThat(s1).isEqualTo(Set.of("aa", "bb", "cc", "dd"));
+    }
+
+    @Test
+    public void testToMapDemo() {
+        final Collector<String, ?, Map<String, File>> col1 = Collectors.toMap(s -> s, s -> new File(s));
+        final Map<String, File> m1 = Stream.of("/home/aaa", "/home/bbb", "/home/ccc").collect(col1);
+        assertThat(m1).hasSize(3);
+        assertThat(m1.get("/home/aaa")).isEqualTo(new File("/home/aaa"));
+        assertThat(m1.get("/home/bbb")).isEqualTo(new File("/home/bbb"));
+        assertThat(m1.get("/home/ccc")).isEqualTo(new File("/home/ccc"));
+        m1.put("/home/ddd", new File("/home/ddd"));
+        assertThat(m1).hasSize(4);
+        assertThat(m1.get("/home/ddd")).isEqualTo(new File("/home/ddd"));
+    }
+
+    @Test
+    public void testToUnmodifiableListDemo() {
+        final Collector<String, ?, List<String>> col1 = Collectors.toUnmodifiableList();
+        List<String> l1 = Stream.of("aa", "bb", "cc").collect(col1);
+        assertThat(l1).isEqualTo(List.of("aa", "bb", "cc"));
+        final Throwable thrown1 = catchThrowable(() -> l1.add("dd"));
+        assertThat(thrown1).isInstanceOf(UnsupportedOperationException.class);
+    }
+
+    @Test
+    public void testToUnmodifiableSetDemo() {
+        final Collector<String, ?, Set<String>> col1 = Collectors.toUnmodifiableSet();
+        final Set<String> s1 = Stream.of("aa", "bb", "cc").collect(col1);
+        assertThat(s1).isEqualTo(Set.of("aa", "bb", "cc"));
+        final Throwable thrown1 = catchThrowable(() -> s1.add("dd"));
+        assertThat(thrown1).isInstanceOf(UnsupportedOperationException.class);
+    }
+
+    @Test
+    public void testToMapUnmodifiableDemo() {
+        final Collector<String, ?, Map<String, File>> col1 = Collectors.toUnmodifiableMap(s -> s, s -> new File(s));
+        final Map<String, File> m1 = Stream.of("/home/aaa", "/home/bbb", "/home/ccc").collect(col1);
+        assertThat(m1).hasSize(3);
+        assertThat(m1.get("/home/aaa")).isEqualTo(new File("/home/aaa"));
+        assertThat(m1.get("/home/bbb")).isEqualTo(new File("/home/bbb"));
+        assertThat(m1.get("/home/ccc")).isEqualTo(new File("/home/ccc"));
+        final Throwable thrown1 = catchThrowable(() -> m1.put("/home/ddd", new File("/home/ddd")));
+        assertThat(thrown1).isInstanceOf(UnsupportedOperationException.class);
+    }
+
+    @Test
+    public void testToCollectionDemo() {
+        final Collector<String, ?, Collection<String>> col1 = Collectors.toCollection(() -> new ArrayList<>());
+        Collection<String> l1 = Stream.of("aa", "bb", "cc").collect(col1);
+        assertThat(l1).isEqualTo(List.of("aa", "bb", "cc"));
+        l1.add("dd");
+        assertThat(l1).isEqualTo(List.of("aa", "bb", "cc", "dd"));
+    }
+
+    @Test
+    public void testToCollectingAndThenDemo() {
+        final Collector<String, ?, List<String>> col1 = Collectors.toList();
+        final Collector<String, ?, List<String>> col2 = Collectors.collectingAndThen(col1,
+                Collections::unmodifiableList);
+        List<String> l1 = Stream.of("aa", "bb", "cc").collect(col2);
+        assertThat(l1).isEqualTo(List.of("aa", "bb", "cc"));
+        final Throwable thrown1 = catchThrowable(() -> l1.add("dd"));
+        assertThat(thrown1).isInstanceOf(UnsupportedOperationException.class);
+    }
+
     /* TODO
-     * collectingAndThen​
      * flatMapping
      * mapping
      * partitioningBy
      * groupingBy
      * reducing
-     * toCollection
-     * toConcurrentMap
-     * toList
-     * toMap
-     * toSet
-     * toUnmodifiableList
-     * toUnmodifiableMap
-     * toUnmodifiableSet
      */
 }
