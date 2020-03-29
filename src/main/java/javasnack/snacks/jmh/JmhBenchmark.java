cf329ed1c98f20fb054d9eb1e040886eda7e3d60
Masahiko Sakamoto <sakamoto.gsyc.3s@gmail.com>
2020-03-29 15:49:01 +0900

    JMH系の実行手順を整理

--- src/main/java/javasnack/snacks/jmh/JmhBenchmark.java
+++ src/main/java/javasnack/snacks/jmh/JmhBenchmark.java
@@ -1,28 +1,41 @@
 package javasnack.snacks.jmh;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Objects;
+
 import javasnack.RunnableSnack;
 
-/* reference:
- * - OpenJDK: jmh
- *  - https://openjdk.java.net/projects/code-tools/jmh/
- * - JMHでJavaのマイクロベンチマークを取得する - 覚えたら書く
- *  - https://blog.y-yuki.net/entry/2016/11/05/000000
- * - JMH - Java Microbenchmark Harness
- *  - http://tutorials.jenkov.com/java-performance/jmh.html
- * 
- * tutorials:
- * - JMH(Java Microbenchmark Harness)のサンプルを動かしながら勉強 - Mitsuyuki.Shiiba
- *   - https://bufferings.hatenablog.com/entry/2018/10/14/232631
- * - 後編：JMH(Java Microbenchmark Harness)のサンプルを動かしながら勉強 - Mitsuyuki.Shiiba
- *   - https://bufferings.hatenablog.com/entry/2018/10/30/014901
- * - Java JMH Benchmark Tutorial – Mkyong.com
- *   - https://mkyong.com/java/java-jmh-benchmark-tutorial/
- * - Microbenchmarking with Java | Baeldung
- *   - https://www.baeldung.com/java-microbenchmark-harness
- */
 public class JmhBenchmark implements RunnableSnack {
     @Override
     public void run(String... args) throws Exception {
-        org.openjdk.jmh.Main.main(args);
+        if (args.length < 1) {
+            System.out.print("Enter JMH args (exit for ENTER):");
+            final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            final String readLine = br.readLine();
+            if (Objects.isNull(readLine)) {
+                return;
+            }
+            if (readLine.trim().length() == 0) {
+                System.err.println("empty args, skip JMH.");
+                return;
+            }
+            args = readLine.trim().split("\\s");
+        }
+        try {
+            org.openjdk.jmh.Main.main(args);
+        } catch (Throwable t) {
+            if ((t instanceof RuntimeException)
+                    && (t.getMessage().contains("Unable to find the resource: /META-INF/BenchmarkList"))) {
+                System.err.println("######");
+                System.err.println(
+                        "###### /META-INF/BenchmarkList not found. try 'mvn clean compile'"
+                                + " (=> jmh-generator-annprocess will generate it :)");
+                System.err.println(
+                        "###### NOTE: Eclipse auto compile nor Project -> Clean does NOT generate that file X(");
+                System.err.println("######");
+            }
+            throw t;
+        }
     }
 }
--- src/main/java/javasnack/snacks/jmh/JmhSamples00HelloWorld.java
+++ src/main/java/javasnack/snacks/jmh/JmhSamples00HelloWorld.java
@@ -4,6 +4,8 @@ import org.openjdk.jmh.annotations.Benchmark;
 
 /* from:
  * https://hg.openjdk.java.net/code-tools/jmh/file/tip/jmh-samples/src/main/java/org/openjdk/jmh/samples/
+ * 
+ * jmh args: JmhSamples00HelloWorld (-f [forks] -i [iterations] ...) 
  */
 public class JmhSamples00HelloWorld {
     @Benchmark
