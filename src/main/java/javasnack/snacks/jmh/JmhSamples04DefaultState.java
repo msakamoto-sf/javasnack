c6f8d7ed247f713e9ddfea378051d7c9feefcc35
Masahiko Sakamoto <sakamoto.gsyc.3s@gmail.com>
2020-03-29 18:27:10 +0900

    add JmhSamples04DefaultState

---
+++ src/main/java/javasnack/snacks/jmh/JmhSamples04DefaultState.java
@@ -0,0 +1,27 @@
+package javasnack.snacks.jmh;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+
+/* copied from:
+ * https://hg.openjdk.java.net/code-tools/jmh/file/tip/jmh-samples/src/main/java/org/openjdk/jmh/samples/
+ * 
+ * jmh args: JmhSamples01HelloWorld (-f [forks] -i [iterations] -wi [warmup-iterations]...) 
+ */
+/*
+ * Fortunately, in many cases you just need a single state object.
+ * In that case, we can mark the benchmark instance itself to be
+ * the @State. Then, we can reference its own fields as any
+ * Java program does.
+ */
+@State(Scope.Thread)
+public class JmhSamples04DefaultState {
+
+    double someDouble = Math.PI;
+
+    @Benchmark
+    public void measure() {
+        someDouble++;
+    }
+}
