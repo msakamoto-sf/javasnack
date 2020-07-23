3fdbbd8064434e588b783b240001dc7266f5708b
sakamoto <sakamoto@securesky-tech.com>
2020-07-23 18:24:58 +0900

    add ojcp Test03Operators, Test04StringOps.

---
+++ src/test/java/javasnack/ojcp/se8silver/Test03Operators.java
@@ -0,0 +1,267 @@
+package javasnack.ojcp.se8silver;
+
+import static javasnack.ojcp.OjcpTools.assertLines;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.assertj.core.data.Offset;
+import org.junit.jupiter.api.Test;
+
+import javasnack.tool.StringWriterPrinter;
+
+public class Test03Operators {
+    @Test
+    public void testArithmeticOperators() {
+        assertThat(10 + 3).isEqualTo(13);
+        assertThat(10 - 3).isEqualTo(7);
+        assertThat(10 * 3).isEqualTo(30);
+        assertThat(10 / 3).isEqualTo(3);
+        assertThat(10 % 3).isEqualTo(1);
+
+        // 単項演算子の順序は前置が「演算したあと評価」、後置が「評価したあと演算」
+        int n1 = 10;
+        assertThat(n1++).isEqualTo(10);
+        assertThat(n1).isEqualTo(11);
+        assertThat(++n1).isEqualTo(12);
+        assertThat(n1).isEqualTo(12);
+
+        assertThat(n1--).isEqualTo(12);
+        assertThat(n1).isEqualTo(11);
+        assertThat(--n1).isEqualTo(10);
+        assertThat(n1).isEqualTo(10);
+
+        int n2 = 10 / 3;
+        assertThat(n2).isEqualTo(3);
+        // 片方が小数点リテラル(double)だと全体がdoubleになるため、intで受けると compile error
+        //int n3 = 10 / 3.0;
+        double n3 = 10 / 3.0;
+        assertThat(n3).isEqualTo(3.333, Offset.offset(0.001));
+
+        // 文字列結合の方が数値演算より優先度高くなる。
+        int a = 10;
+        int b = 20;
+        assertThat("hello" + a).isEqualTo("hello10");
+        assertThat("hello" + a + b).isEqualTo("hello1020");
+        assertThat("hello" + (a + b)).isEqualTo("hello30");
+        assertThat(a + b + "hello").isEqualTo("30hello");
+    }
+
+    @Test
+    public void testAssignmentOperators() {
+        int a = 10;
+        int b = 20;
+        // >#>POINT<#< : 代入演算子は最終的に値として評価できる。
+        int c = (a = b);
+        assertThat(a).isEqualTo(20);
+        assertThat(c).isEqualTo(20);
+        a = 10;
+        c = (a += b);
+        assertThat(a).isEqualTo(30);
+        assertThat(c).isEqualTo(30);
+        a = 10;
+        c = (a -= b);
+        assertThat(a).isEqualTo(-10);
+        assertThat(c).isEqualTo(-10);
+        a = 10;
+        c = (a *= b);
+        assertThat(a).isEqualTo(200);
+        assertThat(c).isEqualTo(200);
+        a = 10;
+        c = (b /= a);
+        assertThat(a).isEqualTo(10);
+        assertThat(b).isEqualTo(2);
+        assertThat(c).isEqualTo(2);
+        a = 3;
+        b = 10;
+        c = (b %= a);
+        assertThat(a).isEqualTo(3);
+        assertThat(b).isEqualTo(1);
+        assertThat(c).isEqualTo(1);
+
+        String d = "hello";
+        String e = (d += "world");
+        assertThat(d).isEqualTo("helloworld");
+        assertThat(e).isEqualTo("helloworld");
+        /* String 型については += 以外は compile error
+        e = (d -= "world");
+        e = (d *= "world");
+        e = (d /= "world");
+        e = (d %= "world");
+        */
+    }
+
+    @Test
+    public void testComparisonOperators() {
+        int a = 10;
+        int b = 10;
+        assertThat(a++ == ++b).isFalse();
+        assertThat(a).isEqualTo(11);
+        assertThat(b).isEqualTo(11);
+
+        assertThat(a++ > ++b).isFalse(); // 11 > 12
+        assertThat(a).isEqualTo(12);
+        assertThat(b).isEqualTo(12);
+
+        assertThat(++a > b++).isTrue(); // 13 > 12
+        assertThat(a).isEqualTo(13);
+        assertThat(b).isEqualTo(13);
+
+        boolean b1 = a++ >= b++;
+        assertThat(b1).isTrue(); // 14 >= 14
+        assertThat(a).isEqualTo(14);
+        assertThat(b).isEqualTo(14);
+
+        assertThat(++a != b++).isTrue(); // 15 != 14
+        assertThat(a).isEqualTo(15);
+        assertThat(b).isEqualTo(15);
+
+        assertThat(++a <= b++).isFalse(); // 16 <= 15
+        assertThat(a).isEqualTo(16);
+        assertThat(b).isEqualTo(16);
+
+        assertThat(a++ < ++b).isTrue(); // 16 < 17
+        assertThat(a).isEqualTo(17);
+        assertThat(b).isEqualTo(17);
+
+        assertThat(a++ != ++b).isTrue(); // 17 != 18
+        assertThat(a).isEqualTo(18);
+        assertThat(b).isEqualTo(18);
+    }
+
+    boolean returnTrue(final StringWriterPrinter swp, final String msg) {
+        swp.out.println(msg);
+        return true;
+    }
+
+    boolean returnFalse(final StringWriterPrinter swp, final String msg) {
+        swp.out.println(msg);
+        return false;
+    }
+
+    @Test
+    public void testLogicalOperators() {
+        StringWriterPrinter swp = new StringWriterPrinter();
+
+        // expr1 && expr2 は、 expr1 が false であれば expr2 を評価しない。
+        assertThat(returnTrue(swp, "aaa") && returnTrue(swp, "bbb")).isTrue();
+        assertLines(swp, "aaa", "bbb");
+        swp.clear();
+        assertThat(returnTrue(swp, "aaa") && returnFalse(swp, "bbb")).isFalse();
+        assertLines(swp, "aaa", "bbb");
+        swp.clear();
+        assertThat(returnFalse(swp, "aaa") && returnTrue(swp, "bbb")).isFalse();
+        assertLines(swp, "aaa");
+        swp.clear();
+        assertThat(returnFalse(swp, "aaa") && returnFalse(swp, "bbb")).isFalse();
+        assertLines(swp, "aaa");
+        swp.clear();
+
+        // >#>POINT<#<:
+        // expr1 & expr2 は、 expr1 が false であっても、expr2 を評価する。
+        assertThat(returnTrue(swp, "aaa") & returnTrue(swp, "bbb")).isTrue();
+        assertLines(swp, "aaa", "bbb");
+        swp.clear();
+        assertThat(returnTrue(swp, "aaa") & returnFalse(swp, "bbb")).isFalse();
+        assertLines(swp, "aaa", "bbb");
+        swp.clear();
+        assertThat(returnFalse(swp, "aaa") & returnTrue(swp, "bbb")).isFalse();
+        assertLines(swp, "aaa", "bbb");
+        swp.clear();
+        assertThat(returnFalse(swp, "aaa") & returnFalse(swp, "bbb")).isFalse();
+        assertLines(swp, "aaa", "bbb");
+        swp.clear();
+
+        // expr1 || expr2 は、どちらかが先にtrueとなるまで評価し、それ以降は評価しない。
+        assertThat(returnTrue(swp, "aaa") || returnTrue(swp, "bbb")).isTrue();
+        assertLines(swp, "aaa");
+        swp.clear();
+        assertThat(returnTrue(swp, "aaa") || returnFalse(swp, "bbb")).isTrue();
+        assertLines(swp, "aaa");
+        swp.clear();
+        assertThat(returnFalse(swp, "aaa") || returnTrue(swp, "bbb")).isTrue();
+        assertLines(swp, "aaa", "bbb");
+        swp.clear();
+        assertThat(returnFalse(swp, "aaa") || returnFalse(swp, "bbb")).isFalse();
+        assertLines(swp, "aaa", "bbb");
+        swp.clear();
+
+        // >#>POINT<#<:
+        // expr1 | expr2 は、必ず両方とも評価する。
+        assertThat(returnTrue(swp, "aaa") | returnTrue(swp, "bbb")).isTrue();
+        assertLines(swp, "aaa", "bbb");
+        swp.clear();
+        assertThat(returnTrue(swp, "aaa") | returnFalse(swp, "bbb")).isTrue();
+        assertLines(swp, "aaa", "bbb");
+        swp.clear();
+        assertThat(returnFalse(swp, "aaa") | returnTrue(swp, "bbb")).isTrue();
+        assertLines(swp, "aaa", "bbb");
+        swp.clear();
+        assertThat(returnFalse(swp, "aaa") | returnFalse(swp, "bbb")).isFalse();
+        assertLines(swp, "aaa", "bbb");
+        swp.clear();
+
+        // XOR
+        assertThat(returnTrue(swp, "aaa") ^ returnTrue(swp, "bbb")).isFalse();
+        assertLines(swp, "aaa", "bbb");
+        swp.clear();
+        assertThat(returnTrue(swp, "aaa") ^ returnFalse(swp, "bbb")).isTrue();
+        assertLines(swp, "aaa", "bbb");
+        swp.clear();
+        assertThat(returnFalse(swp, "aaa") ^ returnTrue(swp, "bbb")).isTrue();
+        assertLines(swp, "aaa", "bbb");
+        swp.clear();
+        assertThat(returnFalse(swp, "aaa") ^ returnFalse(swp, "bbb")).isFalse();
+        assertLines(swp, "aaa", "bbb");
+        swp.clear();
+
+        // NOT
+        assertThat(!returnTrue(swp, "aaa")).isFalse();
+        assertLines(swp, "aaa");
+        swp.clear();
+        assertThat(!returnFalse(swp, "aaa")).isTrue();
+        assertLines(swp, "aaa");
+        swp.clear();
+    }
+
+    @Test
+    public void testBitOperators() {
+        // Java SE 8 Silver の範囲外っぽいけどざっくり見ておく。
+        int a = 0b1100;
+        int b = 0b1010;
+        int c = a & b;
+        assertThat(c).isEqualTo(0b1000);
+        c = a | b;
+        assertThat(c).isEqualTo(0b1110);
+        c = a ^ b; // XOR
+        assertThat(c).isEqualTo(0b110);
+        c = ~a; // NOT = NOT 0b0000_0000_0000_0000_0000_0000_0000_1100
+        assertThat(c).isEqualTo(0b1111_1111_1111_1111_1111_1111_1111_0011); // -13
+
+        a = 0b1100;
+        //a << 2; // compile error
+        c = a << 2;
+        assertThat(c).isEqualTo(0b110000);
+        a = 0b1000_0000_1000_0000_1000_0000_1000_0001;
+        c = a << 2;
+        assertThat(c).isEqualTo(0b00_0000_1000_0000_1000_0000_1000_0001_00);
+        //c = a <<< 2; // compile error (not defined)
+
+        // signed right shift
+        a = 0b1100;
+        //a >> 2; // compile error
+        c = a >> 2;
+        assertThat(c).isEqualTo(0b11);
+        a = 0b1000_0000_1000_0000_1000_0000_1000_0001;
+        c = a >> 2; // >#>POINT<#< : 最左bitが補完される = 符号が維持される。
+        assertThat(c).isEqualTo(0b111000_0000_1000_0000_1000_0000_1000_00);
+
+        // unsigned right shift
+        c = a >>> 2;
+        a = 0b1100;
+        //a >>> 2; // compile error
+        c = a >>> 2;
+        assertThat(c).isEqualTo(0b11);
+        a = 0b1000_0000_1000_0000_1000_0000_1000_0001;
+        c = a >>> 2; // >#>POINT<#< : 最左bitは補完されない = 符号は維持されない。
+        assertThat(c).isEqualTo(0b001000_0000_1000_0000_1000_0000_1000_00);
+    }
+}
---
+++ src/test/java/javasnack/ojcp/se8silver/Test04StringOps.java
@@ -0,0 +1,156 @@
+package javasnack.ojcp.se8silver;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+
+public class Test04StringOps {
+    /* >#>POINT<#< : 位置を2つ取るメソッドはほとんどが(start, end).
+     * start は開始位置のインデックスそのものだが、
+     * end は「end位置の一つ前まで」で統一されている。
+     * 
+     * >#>POINT<#< : String クラスは immutable.
+     * -> concat, replace, trim, substring は元の文字列は変えず、
+     * 操作結果を表す新しい String を返す。
+     */
+
+    @Test
+    public void testStringDemos() {
+        final String s1 = "abc";
+        assertThat(s1.charAt(0)).isEqualTo('a');
+        assertThat(s1.charAt(1)).isEqualTo('b');
+        assertThat(s1.charAt(2)).isEqualTo('c');
+        assertThatThrownBy(() -> {
+            System.out.println(s1.charAt(3));
+        }).isInstanceOf(StringIndexOutOfBoundsException.class);
+
+        final String s2 = s1.concat("def");
+        assertThat(s1).isEqualTo("abc"); // s1 は変化しない。
+        assertThat(s2).isEqualTo("abcdef");
+
+        assertThat(s2.startsWith("abc")).isTrue();
+        assertThat(s2.endsWith("def")).isTrue();
+        assertThat(s2.startsWith("cd", 2)).isTrue(); // 開始位置を指定可能
+
+        assertThat("abcabc".length()).isEqualTo(6);
+        assertThat("abcabc".indexOf('a')).isEqualTo(0);
+        assertThat("abcabc".lastIndexOf('a')).isEqualTo(3);
+        assertThat("abcabc".indexOf('c')).isEqualTo(2);
+        assertThat("abcabc".lastIndexOf('c')).isEqualTo(5);
+
+        String s3a = "abc";
+        String s3b = "abc";
+        // >#>POINT<#< : newを使わない場合、メモリ上にあれば同じ文字列を参照する。
+        assertThat(s3a == s3b).isTrue();
+        // new String() でラップすれば、別インスタンスへの参照となる。
+        s3a = new String("abc");
+        s3b = new String("abc");
+        assertThat(s3a == s3b).isFalse();
+        assertThat(s3a.equals(s3b)).isTrue();
+        // 片方をラップしただけでもやはり別インスタンスへの参照となる。
+        s3a = "abc";
+        s3b = new String("abc");
+        assertThat(s3a == s3b).isFalse();
+        assertThat(s3a.equals(s3b)).isTrue();
+
+        assertThat("ABC".equals("abc".toUpperCase())).isTrue();
+        assertThat("abc".equals("ABC".toLowerCase())).isTrue();
+        assertThat("abc".equalsIgnoreCase("ABC")).isTrue();
+
+        final String s4 = "abcabc";
+        final String s5 = s4.replace('a', 'x');
+        assertThat(s4).isEqualTo("abcabc"); // s4 は変化しない。
+        assertThat(s5).isEqualTo("xbcxbc");
+        final String s6 = s4.replace("ab", "xy");
+        assertThat(s4).isEqualTo("abcabc"); // s4 は変化しない。
+        assertThat(s6).isEqualTo("xycxyc");
+
+        final String s7 = "  abc  ";
+        final String s8 = s7.trim();
+        assertThat(s7).isEqualTo("  abc  "); // s7 は変化しない。
+        assertThat(s8).isEqualTo("abc");
+
+        final String s9 = "hello, world";
+        assertThat(s9.substring(0)).isEqualTo(s9);
+        assertThat(s9.substring(2)).isEqualTo("llo, world");
+        assertThat(s9.substring(2, 8)).isEqualTo("llo, w"); // end(8) の一つ前まで
+    }
+
+    @SuppressWarnings("unlikely-arg-type")
+    @Test
+    public void testStringBuilderDemos() {
+        // 初期容量は16だが、中身の文字列長はまだ空なので0
+        StringBuilder sb1a = new StringBuilder();
+        assertThat(sb1a.length()).isEqualTo(0);
+        // 初期容量を指定しても、中身の文字列長はまだ空なので0
+        sb1a = new StringBuilder(256);
+        assertThat(sb1a.length()).isEqualTo(0);
+        sb1a = new StringBuilder("abc");
+        assertThat(sb1a.length()).isEqualTo(3);
+
+        StringBuilder sb1b = new StringBuilder("abc");
+        // >#>POINT<#< StringBuilder#equals() は中身まではチェックしない。
+        assertThat(sb1a.equals(sb1b)).isFalse();
+        // 以下は "unlikely-arg-type" warning を生成する。
+        assertThat(sb1a.equals(sb1b.toString())).isFalse();
+        assertThat(sb1a.toString().equals(sb1b)).isFalse();
+
+        // 両方を toString() することで、文字列として比較できる。
+        assertThat(sb1a.toString().equals(sb1b.toString())).isTrue();
+
+        /* >#>POINT<#<
+         * StringBuilder は mutable で、文字列を編集する操作では元のデータを変更する。
+         * 以下は戻り値で元の StringBuilder 参照を返すので、method-chainを使える。
+         * - append()
+         * - reverse()
+         * - replace()
+         * - delete()
+         * - deleteCharAt()
+         * 
+         * 以下は戻り値がvoid
+         * - setCharAt()
+         * 
+         * 以下はmutableではなく、substring()結果のStringを返す。
+         * - substring()
+         */
+        final StringBuilder sb2 = sb1a.append("def");
+        assertThat(sb1a == sb2).isTrue(); // 同じ参照を返す。
+        assertThat(sb1a.toString()).isEqualTo("abcdef"); // 元のデータも変更される。
+        assertThat(sb2.toString()).isEqualTo("abcdef");
+
+        final StringBuilder sb3 = sb1a.reverse();
+        assertThat(sb1a == sb3).isTrue(); // 同じ参照を返す。
+        assertThat(sb1a.toString()).isEqualTo("fedcba"); // 元のデータも変更される。
+        assertThat(sb3.toString()).isEqualTo("fedcba");
+
+        final StringBuilder sb4 = new StringBuilder("abcabc");
+        final StringBuilder sb5 = sb4.replace(2, 4, "XYZ");
+        assertThat(sb4 == sb5).isTrue(); // 同じ参照を返す。
+        assertThat(sb4.toString()).isEqualTo("abXYZbc"); // 元のデータも変更される。
+        assertThat(sb5.toString()).isEqualTo("abXYZbc");
+
+        final StringBuilder sb6 = sb4.delete(2, 4);
+        assertThat(sb4 == sb6).isTrue(); // 同じ参照を返す。
+        assertThat(sb4.toString()).isEqualTo("abZbc"); // 元のデータも変更される。
+        assertThat(sb6.toString()).isEqualTo("abZbc");
+
+        final StringBuilder sb7 = sb4.deleteCharAt(1);
+        assertThat(sb4 == sb7).isTrue(); // 同じ参照を返す。
+        assertThat(sb4.toString()).isEqualTo("aZbc"); // 元のデータも変更される。
+        assertThat(sb7.toString()).isEqualTo("aZbc");
+
+        sb4.setCharAt(2, 'W'); // これの戻り値型は void
+        assertThat(sb4.toString()).isEqualTo("aZWc");
+
+        final StringBuilder sb8 = sb4.append("ABCDEF", 1, 4);
+        assertThat(sb4 == sb8).isTrue(); // 同じ参照を返す。
+        assertThat(sb4.toString()).isEqualTo("aZWcBCD"); // 元のデータも変更される。
+        assertThat(sb8.toString()).isEqualTo("aZWcBCD");
+
+        assertThat(sb4.substring(2)).isEqualTo("WcBCD");
+        assertThat(sb4.substring(2, 5)).isEqualTo("WcB");
+        // substring については元データ変更は無し。
+        assertThat(sb4.toString()).isEqualTo("aZWcBCD");
+    }
+}
