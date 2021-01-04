b19a47ca6b90c72626f332666f3430048896ce45
Masahiko Sakamoto <sakamoto.gsyc.3s@gmail.com>
2021-01-04 16:56:26 +0900

    add NfaBackTrackRuntime

---
+++ src/test/java/javasnack/regexp/codezinedemo/NfaBackTrackRuntime.java
@@ -0,0 +1,184 @@
+/*
+ * Copyright 2020 "Masahiko Sakamoto" <sakamoto.gsyc.3s@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package javasnack.regexp.codezinedemo;
+
+import java.util.ArrayDeque;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Queue;
+import java.util.Set;
+import java.util.TreeSet;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+public class NfaBackTrackRuntime {
+    /* 連載記事中には解説が無いが、(6) からDLできるサンプルコードには dfareg/nfa.py として収録されている。
+     * 2種類含まれており、こちらは深さ優先で探索するタイプ。
+     * 深さ優先 : 選択肢を適当に選んで失敗するまで潜り、失敗したら戻って別の選択肢をやり直す (バックトラック)
+     */
+
+    @ToString
+    @EqualsAndHashCode
+    @AllArgsConstructor(staticName = "of")
+    private static class TrackPoint implements Comparable<TrackPoint> {
+        // バックトラック用に、分岐位置の状態とそこからの残りの文字列を保存するコンテナ。
+        final int state;
+        final String left;
+
+        @Override
+        public int compareTo(TrackPoint o) {
+            if (this.state != o.state) {
+                return this.state - o.state;
+            }
+            if (Objects.nonNull(this.left)) {
+                if (Objects.nonNull(o.left)) {
+                    return this.left.compareTo(o.left);
+                } else {
+                    return 1;
+                }
+            } else {
+                // this.left == null
+                return Objects.nonNull(o.left) ? -1 : 0;
+            }
+        }
+    }
+
+    private final Nfa nfa;
+    private int currentState; // エンドマーカ: -1
+    private String left = null; // エンドマーカ: null
+    private final Queue<TrackPoint> branches = new ArrayDeque<>();
+    private Set<TrackPoint> alreadyTracedBranches = new HashSet<>();
+
+    public NfaBackTrackRuntime(final Nfa nfa) {
+        this.nfa = nfa;
+        this.currentState = nfa.start;
+    }
+
+    private boolean transitSingleChar() {
+        if (Objects.isNull(this.left)) {
+            // この時点で残り文字列がnull(エンドマーカ)であれば、これ以上遷移する必要が無いので false を返す。
+            return false;
+        }
+
+        // 次の遷移可能な状態とその時の残り文字列を、分岐ポイントとして保存する。
+        final Set<TrackPoint> setOfNextCandidateBranch = new TreeSet<>();
+
+        // 空文字(ε)で遷移しうる状態を、分岐ポイントに保存する。
+        final Set<Integer> setOfNextStateByEpsilon = this.nfa.transition.apply(currentState, Optional.empty());
+        for (int state : setOfNextStateByEpsilon) {
+            setOfNextCandidateBranch.add(TrackPoint.of(state, left));
+        }
+
+        if (this.left.length() > 0) {
+            // 次の1文字で遷移可能な状態を取り出し、分岐ポイントに保存する。
+            final char nextChar = this.left.charAt(0);
+            if (this.left.length() == 1) {
+                this.left = "";
+            } else {
+                this.left = this.left.substring(1);
+            }
+            final Set<Integer> setOfNextState = this.nfa.transition.apply(currentState, Optional.of(nextChar));
+            for (int state : setOfNextState) {
+                setOfNextCandidateBranch.add(TrackPoint.of(state, left));
+            }
+        } else {
+            // これ以上読む文字が無いので、エンドマーカを分岐ポイントに含める。
+            setOfNextCandidateBranch.add(TrackPoint.of(currentState, null));
+        }
+
+        // 今回の遷移で取り出した分岐ポイントの集合から、既にトレースした分岐ポイントを除去する。
+        setOfNextCandidateBranch.removeAll(alreadyTracedBranches);
+
+        if (setOfNextCandidateBranch.isEmpty()) {
+            // これ以上辿るべき分岐ポイントが見つからなかったため、状態や残り文字列にエンドマーカを設定する。
+            this.currentState = -1;
+            this.left = null;
+        } else {
+            // 辿るべき次の分岐ポイントを取り出し、状態と残り文字列を復元する。
+            final Queue<TrackPoint> queueOfNextCandidateBranch = new ArrayDeque<>(setOfNextCandidateBranch);
+            final TrackPoint next = nextTrackPoint(queueOfNextCandidateBranch);
+            this.currentState = next.state;
+            this.left = next.left;
+            // 残りの分岐ポイントを、次回以降の分岐ポイント一覧に保存する。
+            this.branches.addAll(setOfNextCandidateBranch);
+        }
+        return true;
+    }
+
+    private TrackPoint nextTrackPoint(final Queue<TrackPoint> queueOfTrackPoint) {
+        final TrackPoint next = queueOfTrackPoint.remove(); // あえて要素がなければ例外をthrowさせ、異常検知させる。
+        // 取り出した分岐ポイントを、トレース済みに移す。
+        this.alreadyTracedBranches.add(next);
+        return next;
+    }
+
+    private boolean backtrack() {
+        if (this.branches.isEmpty()) {
+            // セーブされた分岐ポイントが空っぽであれば、backtrack発生せず。
+            return false;
+        }
+        // セーブされた分岐ポイントから1つ取り出し、現在状態と残り文字列を復元する。
+        final TrackPoint nextBranch = nextTrackPoint(this.branches);
+        this.currentState = nextBranch.state;
+        this.left = nextBranch.left;
+        // backtrack発生
+        return true;
+    }
+
+    private boolean isCurrentStatusAcceptable() {
+        // 現在状態がNFAの受理可能状態の集合に含まれていれば、受理可能と判定。
+        return this.nfa.accept.contains(this.currentState);
+    }
+
+    /** バックトラック方式ではプログラムバグで無限ループが発生する危険があるため、安全リミットを設定する。*/
+    private static final int LOOP_LIMITTER = 0xFFFFF;
+
+    private void checkLimit(final int limitter) {
+        if (limitter > LOOP_LIMITTER) {
+            throw new IllegalStateException("loop limit over");
+        }
+    }
+
+    public boolean accept(final String input) {
+        this.left = input;
+        int limitter = 0;
+        while (true) {
+            limitter++;
+            checkLimit(limitter);
+            while (this.transitSingleChar()) {
+                // 適当な経路でNFAを辿れるところまで辿り、分岐ポイントもセーブしておく。
+                limitter++;
+                checkLimit(limitter);
+            }
+            if (this.isCurrentStatusAcceptable()) {
+                // 辿れる範囲で辿り終わった時点で受理可能状態であれば、受理。
+                return true;
+            }
+            if (this.backtrack()) {
+                /* まだ受理可能状態にはなっていないため、バックトラックする
+                 * = 保存された分岐ポイントの1つに立ち戻り、そこから辿り直す。
+                 */
+                continue;
+            }
+            // 受理可能でなく、バックトラックする分岐ポイントも尽きたため、受理できないと判定。
+            return false;
+        }
+    }
+}
---
+++ src/test/java/javasnack/regexp/codezinedemo/NfaBackTrackRuntimeTest.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2020 "Masahiko Sakamoto" <sakamoto.gsyc.3s@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package javasnack.regexp.codezinedemo;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
+import org.junit.jupiter.api.Test;
+
+public class NfaBackTrackRuntimeTest {
+    @Test
+    public void testNfaRuntimeAcceptance() {
+        final Map<StateAndInputCharacter, Set<Integer>> tbl0 = new HashMap<>();
+        tbl0.put(StateAndInputCharacter.of(0, Optional.of('a')), Set.of(1));
+        tbl0.put(StateAndInputCharacter.of(1, Optional.of('b')), Set.of(3)); // ab
+        tbl0.put(StateAndInputCharacter.of(0, Optional.of('c')), Set.of(4));
+        tbl0.put(StateAndInputCharacter.of(4, Optional.of('d')), Set.of(5)); // cd
+        tbl0.put(StateAndInputCharacter.of(0, Optional.empty()), Set.of(6));
+        tbl0.put(StateAndInputCharacter.of(6, Optional.of('e')), Set.of(7));
+        tbl0.put(StateAndInputCharacter.of(7, Optional.of('f')), Set.of(8)); // ef
+        tbl0.put(StateAndInputCharacter.of(7, Optional.empty()), Set.of(9)); // e = ef*
+
+        final NfaStateTransitFunction f0 = (int start, Optional<Character> input) -> {
+            return tbl0.getOrDefault(StateAndInputCharacter.of(start, input), Collections.emptySet());
+        };
+
+        /* Set.of() で unmodifiable な Set になる。
+         * -> 受理可能かの判定処理において、元の受理可能状態の集合が誤って変更されていないことをチェックできる。
+         */
+        final Set<Integer> acceptableStateSet = Set.of(3, 5, 8, 9);
+        final Nfa nfa0 = Nfa.of(f0, 0, acceptableStateSet);
+        var runtime0 = new NfaBackTrackRuntime(nfa0);
+        assertTrue(runtime0.accept("ab"));
+        runtime0 = new NfaBackTrackRuntime(nfa0);
+        assertTrue(runtime0.accept("cd"));
+        runtime0 = new NfaBackTrackRuntime(nfa0);
+        assertTrue(runtime0.accept("e"));
+        runtime0 = new NfaBackTrackRuntime(nfa0);
+        assertTrue(runtime0.accept("ef"));
+
+        // 念の為確認
+        assertThat(acceptableStateSet).isEqualTo(Set.of(3, 5, 8, 9));
+    }
+}
