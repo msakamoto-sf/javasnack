7e3c9fd8cd5493987ec3090059db56893bb94a2d
sakamoto <sakamoto@securesky-tech.com>
2021-01-05 20:33:45 +0900

    NfaBackTrackPerformanceDemoTest で ReDoS 発生パターンの検証に対応

--- src/test/java/javasnack/regexp/codezinedemo/NfaBackTrackPerformanceDemoTest.java
+++ src/test/java/javasnack/regexp/codezinedemo/NfaBackTrackPerformanceDemoTest.java
@@ -16,6 +16,7 @@
 
 package javasnack.regexp.codezinedemo;
 
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.IntStream;
 
 import org.junit.jupiter.api.Test;
@@ -31,6 +32,7 @@ public class NfaBackTrackPerformanceDemoTest {
         final Regexp nfa2dfaCached;
         final Regexp nfa;
         final Regexp nfaBackTracked;
+        final Regexp nfaBackTrackedNoSkip;
         final Pattern jrePattern;
 
         CompiledRegexps(final String regexp) {
@@ -38,11 +40,14 @@ public class NfaBackTrackPerformanceDemoTest {
             this.nfa2dfaCached = Regexp.compileNfa2Dfa(regexp, RegexpOption.ENABLE_NFA2DFA_TRANSITION_CACHE);
             this.nfa = Regexp.compileNfa(regexp);
             this.nfaBackTracked = Regexp.compileNfa(regexp, RegexpOption.NFA_BACKTRACK);
+            this.nfaBackTrackedNoSkip = Regexp.compileNfa(regexp,
+                    RegexpOption.NFA_BACKTRACK,
+                    RegexpOption.DISABLE_NFA_TRACED_BACKTRACK_SKIPPING);
             this.jrePattern = Pattern.compile(regexp);
         }
     }
 
-    String benchmarkForEachWays(final CompiledRegexps compiled, final String matchTo) {
+    static String benchmarkForEachWays(final CompiledRegexps compiled, final String matchTo) {
         final int count = 1000;
 
         final BenchmarkResult r1 = BenchmarkResult.benchmark(() -> {
@@ -57,91 +62,128 @@ public class NfaBackTrackPerformanceDemoTest {
             return compiled.nfa.match(matchTo);
         }, count);
 
+        final AtomicInteger countOfNfaBackTracked = new AtomicInteger(0);
         final BenchmarkResult r4 = BenchmarkResult.benchmark(() -> {
-            return compiled.nfaBackTracked.match(matchTo);
+            final boolean m = compiled.nfaBackTracked.match(matchTo);
+            countOfNfaBackTracked.addAndGet(compiled.nfaBackTracked.getCountOfNfaBackTrack());
+            return m;
         }, count);
 
+        final AtomicInteger countOfNfaBackTrackedNoSkip = new AtomicInteger(0);
         final BenchmarkResult r5 = BenchmarkResult.benchmark(() -> {
+            final boolean m = compiled.nfaBackTrackedNoSkip.match(matchTo);
+            countOfNfaBackTrackedNoSkip.addAndGet(compiled.nfaBackTrackedNoSkip.getCountOfNfaBackTrack());
+            return m;
+        }, count);
+
+        final BenchmarkResult r6 = BenchmarkResult.benchmark(() -> {
             return compiled.jrePattern.matcher(matchTo).matches();
         }, count);
 
-        return r1.totalNanos + "," + r2.totalNanos + "," + r3.totalNanos + "," + r4.totalNanos + "," + r5.totalNanos;
+        return String.join(",",
+                Long.toString(r1.totalNanos),
+                Long.toString(r2.totalNanos),
+                Long.toString(r3.totalNanos),
+                Long.toString(r4.totalNanos),
+                Integer.toString(countOfNfaBackTracked.get()),
+                Long.toString(r5.totalNanos),
+                Integer.toString(countOfNfaBackTrackedNoSkip.get()),
+                Long.toString(r6.totalNanos));
     }
 
-    static final String COLUMN_LABELS = "len,NFA2DFA(cache-off),NFA2DFA(cache-on),NFA(backtrack-off),NFA(backtrack-on),java.util.regex";
+    static final String COLUMN_LABELS = String.join(",",
+            "regexp",
+            "len",
+            "NFA2DFA(cache-off)",
+            "NFA2DFA(cache-on)",
+            "NFA(backtrack-off)",
+            "NFA(backtrack-on+skip)",
+            "countOfBackTracked(+skip)",
+            "NFA(backtrack-on-skip)",
+            "countOfBackTracked(-skip)",
+            "java.util.regex");
 
-    // 実際に試すときは50以上の大きめの値で実験してください。
+    /* O(N^2), O(2^N) で劣化するテストケースが含まれているため、
+     * 10を超える値で検証するときは、時間と検証環境(CPU)に余裕をもたせてください。
+     */
     static final int MAX_LENGTH = 10;
+
+    static void benchmark(final String regexp, final boolean measureAsMatch) {
+        final CompiledRegexps compiled = new CompiledRegexps("(a|a)*");
+        System.out.println(COLUMN_LABELS);
+        IntStream.range(1, MAX_LENGTH + 1).forEach((len) -> {
+            final String matchTo = "a".repeat(len) + (measureAsMatch ? "" : "b");
+            System.out.println(regexp + "," + len + "," + benchmarkForEachWays(compiled, matchTo));
+        });
+    }
 
     @Test
     public void testExponentialDegreeOfAmbiguityDemo1NonMatch() {
         System.out.println(">>>> benchmark demo for EDA : Exponential Degree of Ambiguity(1-not-match)");
-        final CompiledRegexps compiled = new CompiledRegexps("(a|a)*");
-        System.out.println(COLUMN_LABELS);
-        IntStream.range(1, MAX_LENGTH + 1).forEach((len) -> {
-            final String matchTo = "a".repeat(len) + "b";
-            System.out.println(len + "," + benchmarkForEachWays(compiled, matchTo));
-        });
+        benchmark("(a|a)*", false);
         System.out.println("<<<<");
     }
 
     @Test
     public void testExponentialDegreeOfAmbiguityDemo1Match() {
         System.out.println(">>>> benchmark demo for EDA : Exponential Degree of Ambiguity(1-match)");
-        final CompiledRegexps compiled = new CompiledRegexps("(a|a)*");
-        System.out.println(COLUMN_LABELS);
-        IntStream.range(1, MAX_LENGTH + 1).forEach((len) -> {
-            final String matchTo = "a".repeat(len);
-            System.out.println(len + "," + benchmarkForEachWays(compiled, matchTo));
-        });
+        benchmark("(a|a)*", true);
         System.out.println("<<<<");
     }
 
     @Test
     public void testExponentialDegreeOfAmbiguityDemo2NonMatch() {
         System.out.println(">>>> benchmark demo for EDA : Exponential Degree of Ambiguity(2-not-match)");
-        final CompiledRegexps compiled = new CompiledRegexps("(a*)*");
-        System.out.println(COLUMN_LABELS);
-        IntStream.range(1, MAX_LENGTH + 1).forEach((len) -> {
-            final String matchTo = "a".repeat(len) + "b";
-            System.out.println(len + "," + benchmarkForEachWays(compiled, matchTo));
-        });
+        benchmark("(a*)*", false);
         System.out.println("<<<<");
     }
 
     @Test
     public void testExponentialDegreeOfAmbiguityDemo2Match() {
         System.out.println(">>>> benchmark demo for EDA : Exponential Degree of Ambiguity(2-match)");
-        final CompiledRegexps compiled = new CompiledRegexps("(a*)*");
-        System.out.println(COLUMN_LABELS);
-        IntStream.range(1, MAX_LENGTH + 1).forEach((len) -> {
-            final String matchTo = "a".repeat(len);
-            System.out.println(len + "," + benchmarkForEachWays(compiled, matchTo));
-        });
+        benchmark("(a*)*", true);
         System.out.println("<<<<");
     }
 
     @Test
-    public void testInfiniteDegreeOfAmgibuityDemoNonMatch() {
-        System.out.println(">>>> benchmark demo for IDA : Infinite Degree of Amgibuity(Polynomial)(not-match");
-        final CompiledRegexps compiled = new CompiledRegexps("a*a*");
-        System.out.println(COLUMN_LABELS);
-        IntStream.range(1, MAX_LENGTH + 1).forEach((len) -> {
-            final String matchTo = "a".repeat(len) + "b";
-            System.out.println(len + "," + benchmarkForEachWays(compiled, matchTo));
-        });
+    public void testInfiniteDegreeOfAmgibuityDemo1NonMatch() {
+        System.out.println(">>>> benchmark demo for IDA : Infinite Degree of Amgibuity(Polynomial)(1-not-match");
+        benchmark("a*a*", false);
         System.out.println("<<<<");
     }
 
     @Test
-    public void testInfiniteDegreeOfAmgibuityDemoMatch() {
-        System.out.println(">>>> benchmark demo for IDA : Infinite Degree of Amgibuity(Polynomial)(match)");
-        final CompiledRegexps compiled = new CompiledRegexps("a*a*");
-        System.out.println(COLUMN_LABELS);
-        IntStream.range(1, MAX_LENGTH + 1).forEach((len) -> {
-            final String matchTo = "a".repeat(len);
-            System.out.println(len + "," + benchmarkForEachWays(compiled, matchTo));
-        });
+    public void testInfiniteDegreeOfAmgibuityDemo1Match() {
+        System.out.println(">>>> benchmark demo for IDA : Infinite Degree of Amgibuity(Polynomial)(1-match)");
+        benchmark("a*a*", true);
+        System.out.println("<<<<");
+    }
+
+    @Test
+    public void testInfiniteDegreeOfAmgibuityDemo2NonMatch() {
+        System.out.println(">>>> benchmark demo for IDA : Infinite Degree of Amgibuity(Polynomial)(2-not-match");
+        benchmark("a*a*a*", false);
+        System.out.println("<<<<");
+    }
+
+    @Test
+    public void testInfiniteDegreeOfAmgibuityDemo2Match() {
+        System.out.println(">>>> benchmark demo for IDA : Infinite Degree of Amgibuity(Polynomial)(2-match)");
+        benchmark("a*a*a*", true);
+        System.out.println("<<<<");
+    }
+
+    @Test
+    public void testInfiniteDegreeOfAmgibuityDemo3NonMatch() {
+        System.out.println(">>>> benchmark demo for IDA : Infinite Degree of Amgibuity(Polynomial)(3-not-match");
+        benchmark("a*a*a*a*", false);
+        System.out.println("<<<<");
+    }
+
+    @Test
+    public void testInfiniteDegreeOfAmgibuityDemo3Match() {
+        System.out.println(">>>> benchmark demo for IDA : Infinite Degree of Amgibuity(Polynomial)(3-match)");
+        benchmark("a*a*a*a*", true);
         System.out.println("<<<<");
     }
 }
